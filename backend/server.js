8e0a61b81b68e7463ca3e32bb87722d50dd8755f
anton <kamaeff2@gmail.com>
2024-03-11 02:11:05 +0300

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -23,32 +23,6 @@ async function connCreate() {
   });
 }
 
-async function fetchData() {
-  try {
-    const connection = await connCreate();
-    const [users] = await connection.execute(
-      "SELECT COUNT(*) as userCount FROM users"
-    );
-    const [orders] = await connection.execute(
-      "SELECT COUNT(*) as ordersCount FROM orders"
-    );
-
-    const { userCount } = users[0];
-    const { ordersCount } = orders[0];
-    connection.end();
-    return { userCount, ordersCount };
-  } catch (error) {
-    console.error("error creating connection" + error);
-    return 0;
-  }
-}
-
-app.get("/api/user_stat", async (req, res) => {
-  const result = await fetchData();
-
-  res.json({ message: result });
-});
-
 app.get("/api/users_tg", async (req, res) => {
   const { data_members, channelMessages } = await fetchMessages();
 
